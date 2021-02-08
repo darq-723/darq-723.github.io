2d2485fbb246350c28275d7d1029e55b3ebbe71f
Dion Arquisola <76193840+darq-723@users.noreply.github.com>
2021-02-08 19:20:38 +0800

    Add files via upload

---
+++ pass.js
@@ -0,0 +1,43 @@
+// Accounts ko:
+var objPeople = [
+{
+    username: "dragon",
+    password: "deeznuts"
+},
+{
+    username: "Icant",
+    password: "exit_vim" // Seriously, I couldn't exit it in my first try.
+},
+{
+    username: "darq723",
+    password: "samuraicloud123"
+},
+{
+    username: "dionarquisola",
+    password: "samplepass"
+}
+]
+
+// Just like facebook, It gets your info... hahaha pota. ...
+function getInfo(){
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    
+    for(i = 0; i < objPeople.length; i++){
+        if(username == objPeople[i].username && password == objPeople[i].password){
+            console.log(username + " is logged in.")
+            return location.replace("index.html")
+        }
+        else{
+            console.log("Incorrect Credentials")
+        }
+    }
+}
+
+// we must get a function that saves the password and some bullshit, basically a registration system
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,13 @@
+// If you find this, cool. At least the person who found this has basic knowledge about browsers.
+// I am such a nerd.
+console.log("'Must Read': If you find this, cool. At least the person who found it has browser knowledge.")
+// Logs here:
+
+console.log("February 3, 2021: I wonder when will the pandemic end, I'm worried about the situations that is happening right now, I'll chill for a moment. If I drink Coca-Cola, I get sleepy, not active... every... fucking... time.")
+console.log("January 31, 2021: Whatever I'm thinking... the opposite happens, cool right? It seems that my so called 'power' is kinda real tho, I somehow broke the game. I always think of negative thoughts then BAM! profit.")
+console.log("December 22, 2020: It's been days since I last logged in here in my 'secret' journal... I think i'll stream soon. I'll look forward to it. I didn't give up though. I keep moving forward.")
+console.log("December 16, 2020: I'm so tired right now, I stayed up all night so I could attend the mass yesterday, and it was worth it. Yeah, I don't want to lie to myself again, Just like I fooled myself that I had a chance on her. heh, just like old times. I guess, I didn't change. I'm just the good old Dion back in elementary.")
+console.log("December 14, 2020: Yeah, It's me yet again my dearest friend. It seems that I got too carried away by my feelings and yeah, you know what happened, I'm just shy to put it here in my journal cause I'm a pussy, I don't like responsibilities. It's ok, I'm still a good friend. I will not give up 'til the end. Just as I said. I will keep moving forward. Right now, I'm starting to feel insecure again, fuck... I just need some rest.")
+console.log("December 11, 2020: It's me again. The world is getting crazier everyday, I hope that there will be no Apocalypse in 2021.")
+console.log("December 10, 2020: Hello friend. My friend Shek Ming Wong, said that Skye has a 'digger' trait, so I wanted to observe her when It's face to face. So cool... if she doesn't have that trait then, I kinda... like her I guess? maybe it's just the Oxytocin, dopamine, and serotonin acting up in my body I guess, idk, Skye was the only few person who was kind to me, So I guess I developed a crush on her, because of her kindness and uhhhhh s-she's k-kinda cute. But, I mean, Why am I thinking of her when I know she's not thinking of me?")
+console.log("December 9, 2020: It's been 2 months since she last replied to me. i don't know... Maybe I was ghosted. But it's ok, I still support her as her friend.")

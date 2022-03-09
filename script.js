c8015573b3bebae8885fd6f8fe356737462e8a9f
Dion Arquisola <76193840+darq-723@users.noreply.github.com>
2022-03-09 09:37:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,4 @@
 console.log("'Must Read': If you find this, cool. At least the person who found it has browser knowledge.")
 // Logs here:
 
-console.log("February 3, 2021: I wonder when will the pandemic end, I'm worried about the situations that is happening right now, I'll chill for a moment. If I drink Coca-Cola, I get sleepy, not active... every... fucking... time.")
-console.log("January 31, 2021: Whatever I'm thinking... the opposite happens, cool right? It seems that my so called 'power' is kinda real tho, I somehow broke the game. I always think of negative thoughts then BAM! profit.")
-console.log("December 22, 2020: It's been days since I last logged in here in my 'secret' journal... I think i'll stream soon. I'll look forward to it. I didn't give up though. I keep moving forward.")
-console.log("December 16, 2020: I'm so tired right now, I stayed up all night so I could attend the mass yesterday, and it was worth it. Yeah, I don't want to lie to myself again, Just like I fooled myself that I had a chance on her. heh, just like old times. I guess, I didn't change. I'm just the good old Dion back in elementary.")
-console.log("December 14, 2020: Yeah, It's me yet again my dearest friend. It seems that I got too carried away by my feelings and yeah, you know what happened, I'm just shy to put it here in my journal cause I'm a pussy, I don't like responsibilities. It's ok, I'm still a good friend. I will not give up 'til the end. Just as I said. I will keep moving forward. Right now, I'm starting to feel insecure again, fuck... I just need some rest.")
-console.log("December 11, 2020: It's me again. The world is getting crazier everyday, I hope that there will be no Apocalypse in 2021.")
-console.log("December 9, 2020: It's been 2 months since she last replied to me. i don't know... Maybe I was ghosted. But it's ok, I still support her as her friend.")
+console.log("GWAPA GWAPA GD KAY BELSSY YA")
